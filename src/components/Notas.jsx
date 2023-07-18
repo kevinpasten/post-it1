ea0a39871f1926f4823346d425e117d951aed742
ariop <ariop@INACAP.CL>
2023-07-18 17:07:20 -0400

    clase-18

--- src/components/Notas.jsx
+++ src/components/Notas.jsx
@@ -16,20 +16,20 @@ export const Notas = () => {
 
   return (
     <div className="container">
-        <InputDatos agregarNota={AgregarNota} />
-        <div className='d-flex flex-wrap'>
-            {notas.map(nota => (
-                <div key={nota.id} className='col col-lg-3 mt-5'>
-                    <div className={`card mx-auto ${nota.importante ? 'importante' : ''}`}>
-                    <div className='card-body'>
-                        <h3 className='card-title'>{nota.titulo}</h3>
-                        <p className='card-text'>{nota.descripcion}</p>
-                    </div>
-                    <button className='btn btn-sm btn-borrar' onClick={() => BorrarNota(nota.id)}>X</button>
-                    </div>
-                </div>
-            ))}
-        </div>
+      <InputDatos agregarNota={AgregarNota} />
+      <div className='d-flex flex-wrap'>
+        {notas.map(nota => (
+          <div key={nota.id} className='col col-lg-3 mt-5'>
+            <div className={`card mx-auto ${nota.importante ? 'importante' : ''}`}>
+              <div className='card-body'>
+                <h3 className='card-title'>{nota.titulo}</h3>
+                <p className='card-text'>{nota.descripcion}</p>
+              </div>
+              <button className='btn btn-sm btn-borrar' onClick={() => BorrarNota(nota.id)}>X</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
